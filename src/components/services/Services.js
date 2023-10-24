a906e0768d47a10dde213b1ada966a5d9116006a
dineshkush <kushwahd18@gmail.com>
2023-10-24 14:19:15 +0530

    working service page

--- src/components/services/Services.js
+++ src/components/services/Services.js
@@ -1,18 +1,30 @@
-import React from 'react'
+import React from "react";
+import "./Services.css";
+import servicesBg from "../../images/bg-2.jpg";
 
 function Services() {
-
   return (
     <>
-      <section className="services_sec">
+      <section
+        className="services_sec"
+        style={{
+          backgroungImage: `url(${servicesBg})`,
+        }}
+      >
         <div className="container">
           <div className="row">
-            
+            <div className="col-lg-12">
+              <div className="site_title text-center">
+                <h3>
+                  <span>Services</span> For You
+                </h3>
+              </div>
+            </div>
           </div>
         </div>
       </section>
     </>
-  )
+  );
 }
 
-export default Services
+export default Services;
